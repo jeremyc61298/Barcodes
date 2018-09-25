88e3f12f6f407a4a332e9bba0332fc05459773d1
jeremyc61298 <jcampbell2@harding.edu>
2018-09-25 11:58:17 -0500

    Finished project

--- src/barcode.cpp
+++ src/barcode.cpp
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------
+// barcode.cpp
+// Jeremy Campbell
+// This program will convert a number into a corresponding barcode
+// represented by a string with the characters N (narrow bar),
+// n (narrow space), W (wide bar), w, (wide space).
+// ------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -12,14 +19,14 @@ using std::endl;
 
 void readNum(ifstream &fin, stack<short> &s)
 {
-    char digit;
-    fin.get(digit);
+    string number;
+    getline(fin, number);
     
-    while (digit != '\n')
+    for (int i = number.size() - 1; i >= 0; i--)
     {
-        s.push(digit - '0');
-        fin.get(digit);
+        s.push(number[i] - '0');
     }
+    
 }
 
 void hasEvenNumOfDigits(stack<short> &s)
@@ -43,21 +50,41 @@ void initializeNumCodes(string numCodes[])
     numCodes[9] = "nwnwn";
 }
 
-string buildBarcode(stack<short> idNumber)
+string buildBarcode(stack<short> &idNumber, const string numCodes[], const int NUM_CODE_SIZE)
 {
     string barcode;
     const string START_SEQ = "NnNn";
     const string END_SEQ = "WnN";
-    const size_t MAX_DIGITS = 10;
-    string numCodes[MAX_DIGITS];
     
-    //Create an array of all codes for numbers 1-9
-    initializeNumCodes(numCodes);
+    barcode = START_SEQ;
     
-    // Take the 2 top numbers and convert them
+    // Take the 2 top numbers and interleave their codes
+    while (!idNumber.empty())
+    {
+        short barNum = idNumber.top(); idNumber.pop();
+        short spaceNum = idNumber.top(); idNumber.pop();
+        
+        for (int i = 0; i < NUM_CODE_SIZE; i++)
+        {
+            // barNum and spaceNum index numCodes to find the correct code for that number, then
+            // letter by letter, insert the code into the barcode string, making the letters
+            // representing the bars uppercase.
+            barcode += toupper(numCodes[barNum].at(i));
+            barcode += numCodes[spaceNum].at(i);
+        }
+    }
     
-    
+    barcode += END_SEQ;
     return barcode;
+}
+
+void printBarcode(ofstream &fout, string barcode, int size, short top)
+{
+    fout << barcode;
+    
+    // If not the last number, print an end line
+    if(size != 1 || top != 0)
+        fout << endl;
 }
 
 int main() 
@@ -71,13 +98,21 @@ int main()
   
   ofstream fout("barcode.out");
   stack<short> idNumber;
+  
+  //Create an array of all codes for numbers 1-9
+  const size_t MAX_DIGITS = 10;
+  const int NUM_CODE_SIZE = 5;
+  string numCodes[MAX_DIGITS];
+  initializeNumCodes(numCodes);
+  
   readNum(fin, idNumber);
   
-  while (idNumber.top() != 0)
+  // While the number that was just read in is not zero
+  while (idNumber.size() != 1 || idNumber.top() != 0)
   {
       hasEvenNumOfDigits(idNumber);
-      
-      
+      string barcode = buildBarcode(idNumber, numCodes, NUM_CODE_SIZE);
       readNum(fin, idNumber);
+      printBarcode(fout, barcode, idNumber.size(), idNumber.top());
   }
 }
